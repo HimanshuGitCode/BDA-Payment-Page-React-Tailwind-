595df96713f3c1e1b56f12439e9d0f00ad2e6f68
Himanshu Singh <121339915+HimanshuGitCode@users.noreply.github.com>
2024-07-29 13:49:03 +0530

    Add files via upload

---
+++ BDA Payment Page (React+Tailwind)/src/screens/MacbookPro/MacbookPro.jsx
@@ -0,0 +1,135 @@
+import React from "react";
+
+export const MacbookPro = () => {
+  return (
+    <div className="bg-white flex flex-row justify-center w-full">
+      <div className="bg-white w-[1512px] h-[982px]">
+        <div className="relative w-[519px] h-[925px] top-7 left-[513px]">
+          <div className="relative w-[485px] h-[925px] bg-[url(/static/img/subtract.svg)] bg-[100%_100%]">
+            <img className="absolute w-[438px] h-0.5 top-[556px] left-6" alt="Vector" src="/img/vector-1.svg" />
+            <img
+              className="absolute w-[408px] h-px top-[731px] left-[39px] object-cover"
+              alt="Vector"
+              src="/img/vector-2.svg"
+            />
+            <img
+              className="absolute w-[408px] h-px top-[681px] left-[39px] object-cover"
+              alt="Vector"
+              src="/img/vector-3.svg"
+            />
+            <div className="absolute w-[360px] h-[50px] top-[793px] left-[62px] rounded-[45px] shadow-[7px_7px_15px_#00000033] [background:linear-gradient(180deg,rgb(120.48,142.37,84)_0%,rgb(55.24,67.57,34.69)_100%)]">
+              <div className="absolute top-[13px] left-[95px] [font-family:'Inter',Helvetica] font-bold text-white text-xl text-center tracking-[0] leading-[normal] whitespace-nowrap">
+                Process Payment
+              </div>
+            </div>
+            <div className="absolute w-[407px] h-12 top-[250px] left-[38px] bg-white rounded-lg border border-solid border-[#676767]">
+              <div className="absolute top-[11px] left-[13px] opacity-35 [font-family:'Inter',Helvetica] font-normal text-black text-lg tracking-[0] leading-[normal]">
+                Name
+              </div>
+            </div>
+            <div className="absolute w-[407px] h-12 top-[353px] left-[38px] bg-white rounded-lg border border-solid border-[#676767]">
+              <div className="absolute top-3 left-[13px] opacity-35 [font-family:'Inter',Helvetica] font-normal text-black text-lg tracking-[0] leading-[normal]">
+                Email
+              </div>
+            </div>
+            <div className="absolute w-[407px] h-12 top-[460px] left-[38px] bg-white rounded-lg border border-solid border-[#676767]">
+              <div className="absolute top-3 left-[13px] opacity-35 [font-family:'Inter',Helvetica] font-normal text-black text-lg tracking-[0] leading-[normal]">
+                +91 XXXXXXXXXX
+              </div>
+            </div>
+            <div className="absolute top-[599px] left-[49px] [font-family:'Inter',Helvetica] font-bold text-black text-xl tracking-[0] leading-[normal] whitespace-nowrap">
+              Payment Summary-
+            </div>
+            <div className="absolute top-[131px] left-[39px] [font-family:'Inter',Helvetica] font-normal text-[#626262] text-xl tracking-[0] leading-[normal] whitespace-nowrap">
+              You are paying for
+            </div>
+            <p className="absolute top-[223px] left-[38px] [font-family:'Inter',Helvetica] font-normal text-transparent text-base tracking-[0] leading-[normal] whitespace-nowrap">
+              <span className="text-black">Full Name </span>
+              <span className="text-[#d50000]">*</span>
+            </p>
+            <p className="absolute top-[328px] left-[38px] [font-family:'Inter',Helvetica] font-normal text-transparent text-base tracking-[0] leading-[normal] whitespace-nowrap">
+              <span className="text-black">Email </span>
+              <span className="text-[#d50000]">*</span>
+            </p>
+            <p className="absolute top-[435px] left-[38px] [font-family:'Inter',Helvetica] font-normal text-transparent text-base tracking-[0] leading-[normal] whitespace-nowrap">
+              <span className="text-black">Mobile Number </span>
+              <span className="text-[#d50000]">*</span>
+            </p>
+            <div className="absolute top-[162px] left-[39px] [font-family:'Inter',Helvetica] font-bold text-black text-3xl tracking-[0] leading-[normal] whitespace-nowrap">
+              Accelerate with AI
+            </div>
+            <div className="absolute top-[694px] left-12 [font-family:'Inter',Helvetica] font-medium text-black text-lg tracking-[0] leading-[normal]">
+              GST
+            </div>
+            <div className="absolute top-[642px] left-12 [font-family:'Inter',Helvetica] font-medium text-black text-lg tracking-[0] leading-[normal]">
+              Amount
+            </div>
+            <div className="top-[694px] left-[366px] absolute [font-family:'Inter',Helvetica] font-medium text-black text-lg text-right tracking-[0] leading-[normal]">
+              Rs. 3.60
+            </div>
+            <div className="top-[642px] left-[382px] absolute [font-family:'Inter',Helvetica] font-medium text-black text-lg text-right tracking-[0] leading-[normal]">
+              Rs. 20
+            </div>
+            <div className="absolute top-[745px] left-12 [font-family:'Inter',Helvetica] font-medium text-black text-lg tracking-[0] leading-[normal]">
+              Total Payable
+            </div>
+            <p className="absolute top-[858px] left-11 [font-family:'Inter',Helvetica] font-normal text-[#616161] text-[13px] tracking-[0] leading-[normal]">
+              <span className="[font-family:'Inter',Helvetica] font-normal text-[#616161] text-[13px] tracking-[0]">
+                By continuing, you accept our{" "}
+              </span>
+              <span className="underline">Terms</span>
+              <span className="[font-family:'Inter',Helvetica] font-normal text-[#616161] text-[13px] tracking-[0]">
+                ,{" "}
+              </span>
+              <span className="underline">Privacy</span>
+              <span className="[font-family:'Inter',Helvetica] font-normal text-[#616161] text-[13px] tracking-[0]">
+                , and{" "}
+              </span>
+              <span className="underline">Refund Policy</span>
+              <span className="[font-family:'Inter',Helvetica] font-normal text-[#616161] text-[13px] tracking-[0]">
+                .
+              </span>
+            </p>
+            <div className="top-[745px] left-[355px] absolute [font-family:'Inter',Helvetica] font-medium text-black text-lg text-right tracking-[0] leading-[normal]">
+              Rs. 23.60
+            </div>
+            <img className="absolute w-[191px] h-[97px] top-[13px] left-[147px]" alt="Group" src="/img/group-1.png" />
+            <div className="absolute w-[418px] h-[21px] top-[527px] left-9">
+              <div className="relative w-[420px] h-[21px]">
+                <div className="absolute w-[420px] h-5 top-0 left-0">
+                  <div className="relative w-[418px] h-5">
+                    <p className="absolute top-px left-0 [font-family:'Inter',Helvetica] font-normal text-transparent text-base tracking-[0] leading-[normal] whitespace-nowrap">
+                      <span className="text-[#676767]">
+                        We will send update above&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;email
+                        and&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Whatsapp.
+                      </span>
+                      <span className="text-[#d50000]">*</span>
+                    </p>
+                    <img className="absolute w-5 h-5 top-0 left-[305px]" alt="Group" src="/img/group-2.png" />
+                  </div>
+                </div>
+                <img
+                  className="absolute w-[19px] h-[19px] top-0.5 left-[206px] object-cover"
+                  alt="Image"
+                  src="/img/image-2.png"
+                />
+              </div>
+            </div>
+            <div className="absolute w-[171px] h-6 top-[887px] left-[156px]">
+              <div className="relative w-[173px] h-6">
+                <div className="absolute w-[173px] h-6 top-0 left-0">
+                  <div className="relative w-[171px] h-6 bg-[#d8ebff] rounded">
+                    <div className="absolute top-1 left-2 [font-family:'Inter',Helvetica] font-normal text-[#3395ff] text-[13px] tracking-[0] leading-[normal]">
+                      Secured by
+                    </div>
+                  </div>
+                </div>
+                <div className="absolute w-[81px] h-[17px] top-1 left-[85px] bg-[url(/static/img/image-3.png)] bg-cover bg-[50%_50%]" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
